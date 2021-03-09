151d62a490c4ef34304209117bd90d8acd1dece5
bryanDiaz19 <78387383+bryanDiaz19@users.noreply.github.com>
2021-03-09 03:52:31 +0000

    done

--- main.fs
+++ main.fs
@@ -3,18 +3,23 @@ module Assignment
 // Problem 1
 let rec prod (lst:int list) =
     // write your solution here
-    0
+    if List.isEmpty lst
+    then 1
+    else List.head lst * prod (List.tail lst)
 
 // Problem 2
 let rec map f (lst:int list) =
     // write your solution here
-    []
+     match lst with
+      [] -> []
+    | h::t -> (f h)::(map f t)
 
 // Problem 3
 let rec odd (lst:int list) =
     // write your solution here
-    lst
+ lst  
 
+    
 // Problem 4
 let rec filter f lst =
     // write your solution here
